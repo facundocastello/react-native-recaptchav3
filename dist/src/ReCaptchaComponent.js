b38b167d3688d52edba4472bad2c2cb30624d26f
Facundo Castello <facundoacastello@gmail.com>
2022-06-09 10:24:20 -0500

    fix android

--- dist/src/ReCaptchaComponent.js
+++ dist/src/ReCaptchaComponent.js
@@ -42,7 +42,7 @@ class ReCaptchaComponent extends React.PureComponent {
         return React.createElement(react_native_1.View, { style: { flex: 0.0001, width: 0, height: 0 } },
             React.createElement(react_native_webview_1.WebView, { ref: (ref) => {
                     this._webViewRef = ref;
-                }, javaScriptEnabled: true, originWhitelist: ['*'], automaticallyAdjustContentInsets: true, mixedContentMode: 'always', injectedJavaScript: patchPostMessageJsCode, source: {
+                }, javaScriptEnabled: true, androidHardwareAccelerationDisabled: true, originWhitelist: ['*'], automaticallyAdjustContentInsets: true, mixedContentMode: 'always', injectedJavaScript: patchPostMessageJsCode, source: {
                     html: getInvisibleRecaptchaContent(this.props.siteKey, this.props.action),
                     baseUrl: this.props.captchaDomain
                 }, onMessage: (e) => {
